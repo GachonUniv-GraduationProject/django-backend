4a1c598854fb456ee078bc38d310de8eb9f75ffa
Kim-Dong-Jun99 <kimdongjun.business@gmail.com>
2022-08-22 11:56:25 +0900

    tech stack add

--- account/urls.py
+++ account/urls.py
@@ -6,7 +6,8 @@ urlpatterns =[
     path('signup/', views.UserCreate.as_view()),
     path('api-auth/', include('rest_framework.urls')),
     path('check/', views.check_users),
-    path('logintest/', views.logintest),
-    path('signuptest/', views.signuptest)
+    path('logintest/', views.login_test),
+    path('signuptest/', views.signup_test),
+    path('addtech/', views.add_tech_stack)
 
  ]
--- account/views.py
+++ account/views.py
@@ -1,5 +1,5 @@
 from .serializers import UserSerializer
-from .models import User, UserManager
+from .models import User, UserManager, tech_stack
 from rest_framework import generics
 from django.shortcuts import render, HttpResponse, get_object_or_404, redirect
 from django.contrib import auth
@@ -20,7 +20,7 @@ def check_users(request):
     return HttpResponse("<h1>check log</h1>")
 
 
-def logintest(request):
+def login_test(request):
     data = json.loads(request.body)
     print(data['email'])
     print(auth.get_user(request))
@@ -31,11 +31,27 @@ def logintest(request):
     return HttpResponse("check log")
 
 
-def signuptest(request):
+def signup_test(request):
     data = json.loads(request.body)
     print(data['nickname'])
     user = User.objects.create_user(data['email'],data['nickname'],data['name'],data['password'])
     print(user.password)
     get_user = User.objects.get(nickname=data['nickname'])
     print(get_user.password)
+    return HttpResponse("check log")
+
+
+def add_tech_stack(request):
+    data = json.loads(request.body)
+    print(auth.get_user(request))
+    techs = data['techs']
+    for tech in techs:
+        temp_tech = tech_stack.objects.filter(name=tech['name'])
+        if len(temp_tech) == 0:
+            stack_objects_create = tech_stack.objects.create(name=tech['name'])
+        else:
+            stack_objects_create = tech_stack.objects.get(name=tech['name'])
+
+        cur_user = User.objects.get(pk=auth.get_user(request).pk)
+        cur_user.tech_stacks.add(stack_objects_create)
     return HttpResponse("check log")
