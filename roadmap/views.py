d0f6c2c4bea957cb0be338285527614d0aa1f971
Kim-Dong-Jun99 <kimdongjun.business@gmail.com>
2022-08-29 15:41:51 +0900

    roadmap setting done

--- roadmap/views.py
+++ roadmap/views.py
@@ -22,15 +22,35 @@ def add_roadmap_test(request):
 
 def add_roadmap(request):
     data = json.loads(request.body)
+    haebin = {"Frontend","Backend","Android","Blockchain"}
     for key in data.keys():
         print(key)
         for skill in data[key]:
-            if skill['index'] == "-1":
-                if len(skills.objects.filter(name=skill['skill'])) == 0:
-                    base_skill = skills.objects.create(name=skill['skill'])
+            if skill['index'] != "-1":
+                if len(skills.objects.filter(name=skill['baseSkill'], field=key)) == 0:
+                    base_skill = skills.objects.create(name=skill['baseSkill'], field=key)
+                    skill = skills.objects.create(name=skill['skill'], base_id=base_skill.pk, field=key)
                 else:
-                    base_skill = skills.objects.get(skill_name=skill['baseSkill'])
-                skill = skills.objects.create(name=skill['skill'], base_id=base_skill.pk, field=key)
+                    base_skill = skills.objects.filter(name=skill['baseSkill'], field=key)
+                    if len(base_skill) == 0:
+                        skill = skills.objects.create(name=skill['skill'], base_id=base_skill.pk, field=key)
+                    else:
+                        skill = skills.objects.create(name=skill['skill'], base_id=base_skill[len(base_skill)-1].pk,
+                                                      field=key)
+
             else:
-                create_url = url.objects.create(link=skill[''])
+                if key in haebin:
+                    target_skill = skills.objects.filter(name=skill['baseSkill'])
+                    for target in target_skill:
+                        if 'title' in skill.keys():
+                            create_url = url.objects.create(link=skill['URL'], link_name=skill['title'], skill_id=target.pk)
+                        else:
+                            create_url = url.objects.create(link=skill['URL'], link_name="default", skill_id=target.pk)
+                else:
+                    target_skill = skills.objects.filter(name=skill['skill'])
+                    for target in target_skill:
+                        if 'title' in skill.keys():
+                            create_url = url.objects.create(link=skill['URL'], link_name=skill['title'], skill_id=target.pk)
+                        else:
+                            create_url = url.objects.create(link=skill['URL'], link_name="default", skill_id=target.pk)
     return HttpResponse("<h1>check log</h1>")
