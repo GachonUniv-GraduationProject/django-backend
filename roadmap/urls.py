4bcbe8f2404168b4c7a2787a7688cb0b7b02a300
Kim-Dong-Jun99 <kimdongjun.business@gmail.com>
2022-08-30 03:13:03 +0900

    total data input setting done

--- roadmap/urls.py
+++ roadmap/urls.py
@@ -3,7 +3,6 @@ from . import views
 from rest_framework import urls
 
 urlpatterns =[
-    path('add_test/', views.add_roadmap_test),
     path('add/', views.add_roadmap)
 
  ]
--- roadmap/views.py
+++ roadmap/views.py
@@ -1,23 +1,9 @@
 from django.shortcuts import render,HttpResponse
 import json
-from .models import JavaScript,js_url, skills, url
+from .models import skills, url
 
 
 # Create your views here.
-def add_roadmap_test(request):
-    data = json.loads(request.body)
-#    print(data['JavaScript'])
-#    print(data['URL'])
-    for skill in data['JavaScript']:
-        if len(JavaScript.objects.filter(skill_name=skill['baseSkill'])) == 0:
-            base_skill = JavaScript.objects.create(skill_name=skill['baseSkill'])
-        else:
-            base_skill = JavaScript.objects.get(skill_name=skill['baseSkill'])
-        skill = JavaScript.objects.create(skill_name=skill['skill'], base_id=base_skill.pk)
-
-    for url in data['URL']:
-        create_url = js_url.objects.create(link=url['URL'], link_name=url['title'], skill_id=JavaScript.objects.get(skill_name=url['skill']).pk)
-    return HttpResponse("<h1>check log</h1>")
 
 
 def add_roadmap(request):
@@ -40,14 +26,17 @@ def add_roadmap(request):
 
             else:
                 if key in haebin:
-                    target_skill = skills.objects.filter(name=skill['baseSkill'])
+                    target_skill = skills.objects.filter(name=skill['baseSkill'], field=key)
                     for target in target_skill:
                         if 'title' in skill.keys():
                             create_url = url.objects.create(link=skill['URL'], link_name=skill['title'], skill_id=target.pk)
                         else:
                             create_url = url.objects.create(link=skill['URL'], link_name="default", skill_id=target.pk)
                 else:
-                    target_skill = skills.objects.filter(name=skill['skill'])
+                    if key == "URL":
+                        target_skill = skills.objects.filter(name=skill['skill'], field=skill['field'])
+                    else:
+                        target_skill = skills.objects.filter(name=skill['skill'], field=key)
                     for target in target_skill:
                         if 'title' in skill.keys():
                             create_url = url.objects.create(link=skill['URL'], link_name=skill['title'], skill_id=target.pk)
