5b375842d30da1e76972ac5e94d54418d08a120c
Kim-Dong-Jun99 <kimdongjun.business@gmail.com>
2022-10-24 00:03:07 +0900

    csrf refactor

--- djangoProject/settings.py
+++ djangoProject/settings.py
@@ -138,4 +138,4 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
-CSRF_TRUSTED_ORIGINS = ['https://gpdevapp.com','http://*.', 'https://*.']
+CSRF_TRUSTED_ORIGINS = ['https://gpdevapp.com','http://*.com', 'https://*.com']
