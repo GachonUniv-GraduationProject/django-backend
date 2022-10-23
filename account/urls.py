d56bed2152982d600c854997348b1d2d69c9b4d1
Kim-Dong-Jun99 <kimdongjun.business@gmail.com>
2022-10-23 22:55:47 +0900

    refactoring

--- account/urls.py
+++ account/urls.py
@@ -6,14 +6,17 @@ urlpatterns =[
     # 회원가입, 로그인을 관리
     # 회원가입은 signup으로 post 요청이 오면 처리해줄것임
     # 로그인도 login으로 post 요청, 로그아웃은 logout으로 post 요청오게끔
-    # 기술 스택은 tech, get으로 오면 유저의 기술 스택 가져오기, post면 추가하기, update면 수정, delete으로 삭제되게
-    path('signup/', views.UserCreate.as_view()),
-    path('api-auth/', include('rest_framework.urls')),
-    path('check/', views.check_users),
-    path('logintest/', views.login_test),
-    path('signuptest/', views.signup_test),
-    path('addtech/', views.add_tech_stack),
-    path('', views.user_check),
-    path('logout/', views.logout)
-
+    # 기술 스택은 tech, get으로 오면 유저의 기술 스택 가져오기, post면 추가하기, patch면 수정, delete으로 삭제되게
+    # path('signup/', views.UserCreate.as_view()),
+    # path('api-auth/', include('rest_framework.urls')),
+    # path('check/', views.check_users),
+    # path('logintest/', views.login_test),
+    # path('signuptest/', views.signup_test),
+    # path('addtech/', views.add_tech_stack),
+    # path('', views.user_check),
+    # path('logout/', views.logout)
+    path('',views.IndexView.as_view(), name="index"),
+    path('login/',views.LoginView.as_view(), name="index"),
+    path('signup/', views.SignupView.as_view(), name="index"),
+    path('tech/', views.TechView.as_view(), name="index")
  ]
--- account/views.py
+++ account/views.py
@@ -3,73 +3,152 @@ from .models import User, UserManager, tech_stack
 from rest_framework import generics
 from django.shortcuts import render, HttpResponse, get_object_or_404, redirect
 from django.contrib import auth
+from django.views import View
 import json
 
 
-class UserCreate(generics.CreateAPIView):
-    queryset = User.objects.all()
-    serializer_class = UserSerializer
+class IndexView(View):
+
+    def get(self, request):
+        cur_user = auth.get_user(request)
+        return HttpResponse("<h1>cur user : " + str(cur_user.pk) + "</h1>")
 
 
-def check_users(request):
-    users = User.objects.all()
-    for user in users:
+class LoginView(View):
+
+    def get(self, request):
+        print(auth.get_user(request))
+        cur_user = auth.get_user(request)
+        return HttpResponse("<h1>cur user : " + cur_user.nickname + "</h1>")
+
+    def post(self, request):
+        data = json.loads(request.body)
+        print(data['email'])
+        print(auth.get_user(request))
+        user = User.objects.get(nickname=data['nickname'])
         print(user)
-        print(type(user))
-        print(user.id)
-    return HttpResponse("<h1>check log</h1>")
+        auth.login(request, user)
+        print(auth.get_user(request))
+        return HttpResponse("check log, new user has login")
+
+    def delete(self, request):
+        past_user = auth.get_user(request).nickname
+        auth.logout(request)
+        cur_user = auth.get_user(request)
+        print(cur_user)
+
+        return HttpResponse("<h1>" + past_user + " log out</h1>")
 
 
-def login_test(request):
-    data = json.loads(request.body)
-    print(data['email'])
-    print(auth.get_user(request))
-    user = User.objects.get(nickname=data['nickname'])
-    print(user)
-    auth.login(request, user)
-    print(auth.get_user(request))
-    return HttpResponse("check log")
-
-
-def signup_test(request):
-    data = json.loads(request.body)
-    print(data['nickname'])
-    if len(User.objects.filter(email=data['email'])) == 0:
-        user = User.objects.create_user(data['email'],data['nickname'],data['name'],data['password'])
-        print(user.password)
-    else:
-        print("duplicate user")
-    get_user = User.objects.get(nickname=data['nickname'])
-    print(get_user.password)
-    return HttpResponse("check log")
-
-
-def add_tech_stack(request):
-    data = json.loads(request.body)
-    print(auth.get_user(request))
-    techs = data['techs']
-    for tech in techs:
-        temp_tech = tech_stack.objects.filter(name=tech['name'])
-        if len(temp_tech) == 0:
-            stack_objects_create = tech_stack.objects.create(name=tech['name'])
+class SignupView(View):
+    def post(self, request):
+        data = json.loads(request.body)
+        print(data['nickname'])
+        if len(User.objects.filter(email=data['email'])) == 0:
+            user = User.objects.create_user(data['email'], data['nickname'], data['name'], data['password'])
+            print(user.password)
         else:
-            stack_objects_create = tech_stack.objects.get(name=tech['name'])
+            print("duplicate user")
+            return HttpResponse("duplicate user")
+        get_user = User.objects.get(nickname=data['nickname'])
+        print(get_user.password)
+        return HttpResponse(get_user.email+" created")
 
+
+class TechView(View):
+
+    def get(self, request):
         cur_user = User.objects.get(pk=auth.get_user(request).pk)
-        cur_user.tech_stacks.add(stack_objects_create)
-    return HttpResponse("check log")
+        return HttpResponse(cur_user.tech_stacks)
 
+    def post(self, request):
+        data = json.loads(request.body)
+        print(auth.get_user(request))
+        techs = data['techs']
+        for tech in techs:
+            temp_tech = tech_stack.objects.filter(name=tech['name'])
+            if len(temp_tech) == 0:
+                stack_objects_create = tech_stack.objects.create(name=tech['name'])
+            else:
+                stack_objects_create = tech_stack.objects.get(name=tech['name'])
 
-def user_check(request):
-    print(auth.get_user(request))
-    cur_user = auth.get_user(request)
-    return HttpResponse("<h1>cur user : "+cur_user.nickname+"</h1>")
+            cur_user = User.objects.get(pk=auth.get_user(request).pk)
+            cur_user.tech_stacks.add(stack_objects_create)
+        return HttpResponse("check log")
+# class UserCreate(generics.CreateAPIView):
+#     queryset = User.objects.all()
+#     serializer_class = UserSerializer
+#
+#
+# # 전체 유저 가져오는 함수
+# def check_users(request):
+#     users = User.objects.all()
+#     for user in users:
+#         print(user)
+#         print(type(user))
+#         print(user.id)
+#     return HttpResponse("<h1>check log</h1>")
+#
+#
+# # 이메일과 닉네임을 전달받고, 닉네임으로 유저 찾아서 로그인하는 함수
+# def login_test(request):
+#     data = json.loads(request.body)
+#     print(data['email'])
+#     print(auth.get_user(request))
+#     user = User.objects.get(nickname=data['nickname'])
+#     print(user)
+#     auth.login(request, user)
+#     print(auth.get_user(request))
+#     return HttpResponse("check log, new user has login")
+#
+#
+# # 회원가입 함수, 중복 유저 검증까지 어느정도 한다.
+# def signup_test(request):
+#     data = json.loads(request.body)
+#     print(data['nickname'])
+#     if len(User.objects.filter(email=data['email'])) == 0:
+#         user = User.objects.create_user(data['email'], data['nickname'], data['name'], data['password'])
+#         print(user.password)
+#     else:
+#         print("duplicate user")
+#     get_user = User.objects.get(nickname=data['nickname'])
+#     print(get_user.password)
+#     return HttpResponse("check log")
+#
+#
+# # 사용자 기술 스택 추가하는 함수
+# def add_tech_stack(request):
+#     data = json.loads(request.body)
+#     print(auth.get_user(request))
+#     techs = data['techs']
+#     for tech in techs:
+#         temp_tech = tech_stack.objects.filter(name=tech['name'])
+#         if len(temp_tech) == 0:
+#             stack_objects_create = tech_stack.objects.create(name=tech['name'])
+#         else:
+#             stack_objects_create = tech_stack.objects.get(name=tech['name'])
+#
+#         cur_user = User.objects.get(pk=auth.get_user(request).pk)
+#         cur_user.tech_stacks.add(stack_objects_create)
+#     return HttpResponse("check log")
+#
+#
+# # 현재 유저를 확인하는 함수
+# def user_check(request):
+#     print(auth.get_user(request))
+#     cur_user = auth.get_user(request)
+#     return HttpResponse("<h1>cur user : " + cur_user.nickname + "</h1>")
+#
+#
+# # 로그아웃하는 함수
+# def logout(request):
+#     past_user = auth.get_user(request).nickname
+#     auth.logout(request)
+#     cur_user = auth.get_user(request)
+#     print(cur_user)
+#
+#     return HttpResponse("<h1>" + past_user + " log out</h1>")
 
-
-def logout(request):
-    past_user = auth.get_user(request).nickname
-    auth.logout(request)
-    cur_user = auth.get_user(request)
-    print(cur_user)
-
-    return HttpResponse("<h1>"+past_user+" log out</h1>")
+# 리팩터링을 어떻게 진행해야될까
+# https://eunjin3786.tistory.com/133
+# 위 블로그 참고해서 만들면 될 것 같다.
