cb7af37a69dbdb8a47aee7f16bdff2a0b79c8226
Kim-Dong-Jun99 <kimdongjun.business@gmail.com>
2022-11-29 14:14:48 +0900

    트렌드 리턴 로직 변경(backend, frontend 분야만 리턴 가능)

--- crawling/views.py
+++ crawling/views.py
@@ -2,29 +2,32 @@ from django.http import JsonResponse
 from django.shortcuts import HttpResponse
 import json
 from .models import data, keyword, trend
-from .serializers import dataSerializer, keywordSerializer, trendSerializer
+from roadmap.models import skills
+from .serializers import dataSerializer, keywordSerializer
 from rest_framework import viewsets
 
 # 취업공고의 position을 보고, 거기서부터 필드명을 추출해야할듯?
-field_keywords = {"Frontend": ["front-end", "frontend", "프론트엔드", "웹",
-                               "프론트", "풀스택", "리엑트", "react", "vue"],
-                  "Backend": ["back-end", "backend", "devops", "백엔드", "서버", "네트워크", "풀스택",
-                              "웹", "server", "cloud", "데브옵스", "자바", "spring"],
-                  "Android": ["안드로이드", "android", "앱"],
-                  "Blockchain": ["blockchain", "block-chain", "블록체인"],
-                  "AI": ["인공지능", "ai"],
-                  "Data Science": ["데이터"],
-                  "Machine Learning": ["머신러닝", "머신 러닝", "machine learning", "ml"],
-                  "Deep Learning": ["딥러닝", "딥 러닝", "deep learning"],
-                  "Data Engineer": ["dba", ],
-                  "BigData Engineer": ["빅데이터"],
-                  "Game Client": ["unity", "언리얼", "유니티"],
-                  "Game Server": ["게임"],
-                  "JavaScript": ["javascript", "자바스크립트", "jsp", "js"],
-                  "Java": ["java", "자바"],
-                  "Python": ["python", "파이썬"],
-                  "React": ["react", "리엑트"],
-                  "Node.js": ["node", "node.js", "노드"]}
+field_keywords = {
+    "Frontend": ["front-end", "frontend", "프론트엔드", "웹",
+                 "프론트", "리엑트", "react", "vue"],
+    "Backend": ["back-end", "backend", "devops", "백엔드", "서버", "네트워크",
+                "웹", "server", "cloud", "데브옵스", "자바", "spring"],
+    # "Android": ["안드로이드", "android", "앱"],
+    # "Blockchain": ["blockchain", "block-chain", "블록체인"],
+    # "AI": ["인공지능", "ai"],
+    # "Data Science": ["데이터"],
+    # "Machine Learning": ["머신러닝", "머신 러닝", "machine learning", "ml"],
+    # "Deep Learning": ["딥러닝", "딥 러닝", "deep learning"],
+    # "Data Engineer": ["dba", ],
+    # "BigData Engineer": ["빅데이터"],
+    # "Game Client": ["unity", "언리얼", "유니티"],
+    # "Game Server": ["게임"],
+    # "JavaScript": ["javascript", "자바스크립트", "jsp", "js"],
+    # "Java": ["java", "자바"],
+    # "Python": ["python", "파이썬"],
+    # "React": ["react", "리엑트"],
+    # "Node.js": ["node", "node.js", "노드"]
+}
 
 
 # Create your views here.
@@ -49,14 +52,38 @@ def get_field(request):
 
 
 def get_trend(request, field):
-    query = trend.objects.filter(field_name=field)
+    query_trend = trend.objects.filter(field_name=field)
+    query_roadmap = skills.objects.filter(field=field)
+    roadmap_result = {}
+    for q in query_roadmap:
+        if q.base is None:
+            roadmap_result[q.name] = {"base": q.field, "child": []}
+        else:
+            if q.base.name in roadmap_result.keys():
+                roadmap_result[q.name] = {"base": q.base.name, "child": []}
+                roadmap_result[q.base.name]["child"].append(q.name)
+
+        print(q)
     result = {}
-    for q in query:
+    for q in query_trend:
         for kw in q.keywords.all():
-            if kw.name not in result:
-                result[kw.name] = 1
-            else :
-                result[kw.name] += 1
+            for roadmap_key in roadmap_result.keys():
+                split_list = roadmap_key.split()
+                for split in split_list:
+                    if kw.name in split and len(kw.name) > 1:
+                        if roadmap_result[roadmap_key]["base"] in result:
+                            if kw.name in result[roadmap_result[roadmap_key]["base"]].keys():
+                                result[roadmap_result[roadmap_key]["base"]][kw.name] += 1
+                            else:
+                                result[roadmap_result[roadmap_key]["base"]][kw.name] = 1
+                        else:
+                            result[roadmap_result[roadmap_key]["base"]] = {}
+                            result[roadmap_result[roadmap_key]["base"]][kw.name] = 1
+
+            # if kw.name not in result:
+            #     result[kw.name] = 1
+            # else:
+            #     result[kw.name] += 1
     return JsonResponse(result)
 
 
@@ -66,13 +93,16 @@ def test(request):
 
 def trend_update(request):
     crawling_datas = data.objects.all()
+    redun_check = {}
     for crawling_data in crawling_datas:
         split_list = crawling_data.position.split()
         print(split_list)
+        for field in field_keywords.keys():
+            redun_check[field] = False
         for word in split_list:
             for field in field_keywords.keys():
                 for kw in field_keywords[field]:
-                    if kw in word:
+                    if kw in word and not redun_check[field]:
                         for key in crawling_data.keywords.all():
                             temp_trend = trend.objects.create(field_name=field)
                             temp_read = keyword.objects.filter(name=key.name)
@@ -81,6 +111,10 @@ def trend_update(request):
                             else:
                                 add_key = keyword.objects.get(name=key.name)
                             temp_trend.keywords.add(add_key)
+                        redun_check[field] = True
+                    elif redun_check[field]:
+                        break
+
     return JsonResponse({"trend": "test"})
 
 
