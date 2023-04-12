1344e2a2ea6923bd7e5511e49172e1e2071c758f
Kim-Dong-Jun99 <kimdongjun.business@gmail.com>
2023-04-12 13:57:52 +0900

    로드맵 주석 작성

--- roadmap/urls.py
+++ roadmap/urls.py
@@ -26,14 +26,19 @@ url_detail = UrlViewSet.as_view({
 })
 
 urlpatterns = [
-    # 로드맵 추가도 post로하고, 조회는 get으로,
-    # 경로도 root 경로로 변경하자
+    # 로드맵 스킬 전체 조회
     path('skill', skill_list),
+    # 로드맵 스킬 단일 조회
     path('skill/<int:pk>', skill_detail),
+    # 로드맵 교육영상 전체 조회
     path('url', url_list),
+    # 로드맵 교육영상 단일 조회
     path('url/<int:pk>', url_detail),
+    # 로드맵 트리구조로 조회 API
     path('tree', views.get_fields),
+    # 로드맵 분야별 조회
     path('tree/<field>', views.get_dict),
+    # 사용자 현재 레벨 조회 API
     path('level', views.update_level)
     # path('add/', views.add_roadmap)
 
